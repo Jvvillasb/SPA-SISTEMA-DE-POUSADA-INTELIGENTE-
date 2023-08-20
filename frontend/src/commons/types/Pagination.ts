759cd816c61ca50bae1fb10e9e430cb9fab098b6
luis291099 <luis291099@gmail.com>
2023-08-20 11:53:03 -0300

    feature: add client service

---
+++ frontend/src/commons/types/Pagination.ts
@@ -0,0 +1,12 @@
+export type Pagination<T> = {
+    content: Array<T>
+    empty: boolean
+    first: boolean
+    last: boolean
+    number: number
+    numberOfElements: number
+    size: number
+    sort: {sorted: boolean, empty: boolean, unsorted: boolean}
+    totalElements: number
+    totalPages: number
+}
