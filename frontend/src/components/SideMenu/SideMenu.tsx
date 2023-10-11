fa99831779ba2f640164337151ba9a51d86ae601
Vinicius Fabiano Silva <57044645+viniciusfabsilva@users.noreply.github.com>
2023-10-11 15:22:00 -0300

    Adiciona navegação pelo side menu (#3)
    Co-authored-by: viniciussilva <vinicius.silva@eduqo.com.br>

--- frontend/src/components/SideMenu/SideMenu.tsx
+++ frontend/src/components/SideMenu/SideMenu.tsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Styles from './SideMenu.module.scss';
 
 const SideMenu = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [selectedItem, setSelectedItem] = useState('');
+
+  useEffect(() => {
+    if (location.pathname.includes('/list')) {
+      setSelectedItem('clientes');
+    } else if (location.pathname.includes('/caravanas')) {
+      setSelectedItem('Caravanas');
+    }
+  }, [location]);
 
   const handleItemClick = (item: string) => {
     setSelectedItem(item);
+    if (item === 'clientes') {
+      navigate('/list');
+    } else if (item === 'Caravanas') {
+      navigate('/');
+    }
   };
 
   return (
     <Box className={Styles.sideMenu} width="204px">
       <div className={Styles.sideMenuOptions}>
         <a
-          className={`${Styles.options} ${selectedItem === 'atendimentos' ? Styles.selected : ''}`}
-          onClick={() => handleItemClick('atendimentos')}
+          className={`${Styles.options} ${selectedItem === 'Caravanas' ? Styles.selected : ''}`}
+          onClick={() => handleItemClick('Caravanas')}
         >
-          Atendimentos
+          Caravanas
         </a>
         <a
           className={`${Styles.options} ${selectedItem === 'clientes' ? Styles.selected : ''}`}
