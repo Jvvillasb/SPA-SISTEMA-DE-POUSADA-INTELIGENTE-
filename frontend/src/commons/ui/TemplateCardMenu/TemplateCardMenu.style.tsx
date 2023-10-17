c8444b3dd87c8ca7e19648ebc5feaf634705e48f
luis291099 <luis291099@gmail.com>
2023-10-17 20:27:37 -0300

    feature: add styled TemplateCardMenu component

---
+++ frontend/src/commons/ui/TemplateCardMenu/TemplateCardMenu.style.tsx
@@ -0,0 +1,10 @@
+import { MenuButton as ChakraMenuButton } from "@chakra-ui/react";
+
+import styled from "@emotion/styled";
+
+export const TemplateCardMenuButton = styled(ChakraMenuButton)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  color: #81868f;
+`;
