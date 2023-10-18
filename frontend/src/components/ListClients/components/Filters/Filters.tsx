54536c4333add9a3a3134eda6c0f7376e5aa5848
viniciussilva <vinicius.silva@eduqo.com.br>
2023-10-17 23:18:42 -0300

    feat: corrigindo arquivo que não estava sendo mais utilizado e formatando código do filters

--- frontend/src/components/ListClients/components/Filters/Filters.tsx
+++ frontend/src/components/ListClients/components/Filters/Filters.tsx
@@ -1,16 +1,19 @@
-import { FormEvent, useRef } from "react";
-import { useDebounce } from "../../../../commons/hooks/useDebounce";
-import useStore from "../../../../store/index";
-import { StyledInput } from "./Filters.styles";
+import { useDebounce } from '../../../../commons/hooks/useDebounce';
+import useStore from '../../../../store/index';
+import { StyledInput } from './Filters.styles';
 
 const Filters = () => {
-
-    const {setSearchString, searchString, fetchClients} = useStore(state => ({setSearchString: state.setSearchString, searchString: state.searchString, fetchClients: state.fetchClients}));
-
+    const { setSearchString, searchString, fetchClients } = useStore(
+        (state) => ({
+            setSearchString: state.setSearchString,
+            searchString: state.searchString,
+            fetchClients: state.fetchClients,
+        })
+    );
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setSearchString(e.target.value);
-            debouncedHandleInputChange();
+        setSearchString(e.target.value);
+        debouncedHandleInputChange();
     };
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -18,15 +21,20 @@ const Filters = () => {
             e.preventDefault();
             fetchClients();
         }
-    }
+    };
 
     const debouncedHandleInputChange = useDebounce(fetchClients, 500);
 
     return (
         <>
-            <StyledInput type="text" value={searchString} onChange={handleInputChange} onKeyDown={handleKeyDown}/>
+            <StyledInput
+                type="text"
+                value={searchString}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+            />
         </>
-    )
-}
+    );
+};
 
-export default Filters
+export default Filters;
