a70b5e234b28770a69c96b099d7cbcf0fd4251e7
Vinicius Fabiano <viniciusfabi1@hotmail.com>
2023-11-19 16:40:00 -0300

    fix: ajustes de partes do códigop

--- frontend/src/components/Forms/Client/CreateClient/GerencialInfoRegister.tsx
+++ frontend/src/components/Forms/Client/CreateClient/GerencialInfoRegister.tsx
@@ -29,15 +29,6 @@ const GerencialInfoRegister: React.FC<GerencialInfoRegisterProps> = ({
                 </Label>
 
                 <Label>
-                    <span>Data de Saída:</span>
-                    <Input
-                        {...register('dataSaida')}
-                        placeholder="Data de Saída"
-                        type="date"
-                    />
-                </Label>
-
-                <Label>
                     <span>Evento:</span>
                     <Input
                         {...register('evento', {
