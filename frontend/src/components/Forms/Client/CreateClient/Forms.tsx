042ecd3d6b6773a0a5416eb2ff1a1086e1957ba2
Vinicius Fabiano <viniciusfabi1@hotmail.com>
2023-11-19 17:43:26 -0300

    fix: forms

--- frontend/src/components/Forms/Client/CreateClient/Forms.tsx
+++ frontend/src/components/Forms/Client/CreateClient/Forms.tsx
@@ -15,7 +15,6 @@ interface ClientFormProps {
 const validateData = (data: Client) => {
     data.dataEntrada = formatDateToBR(data.dataEntrada);
     data.dataNascimento = formatDateToBR(data.dataNascimento);
-    data.dataSaida = formatDateToBR(data.dataSaida);
 };
 
 const CreateClientForm: React.FC<ClientFormProps> = ({
