533eab55347fb70a801e80b83a5f3606db4f25bc
vinicius <viniciusfabi1@hotmail.com>
2024-09-23 20:38:42 -0300

    fix: cors

--- backend/src/main/java/com/example/dspousada/config/CorsConfig.java
+++ backend/src/main/java/com/example/dspousada/config/CorsConfig.java
@@ -8,7 +8,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("*");
+        .allowedOrigins("https://sistemadepousadaintelegente.netlify.app")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
     }
 }
